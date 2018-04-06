7d162c10598be1e54ec3378cf0b11957b029d670
Ong Jian Wei <showershower59@gmail.com>
2018-04-06 13:47:31 +0800

    git push  time variable value changed to 1200

--- gitPull.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-	cout << "perform git pull" << endl;
-	return 0;
-}
--- greeting.cpp
+++ greeting.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 	
-	int time = 12000;
+	int time = 1200;
 	
 	cout << "Time now is" << time << endl;
 }
